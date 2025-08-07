a613424c52c90246cfbcaf3aa0f5c32a69255751
Tejus Kandipilli <tejusking12447@gmail.com>
2025-08-07 16:15:36 +0530

    Finished project by adding database and endpoints

--- src/main/java/com/tejuskandipilli/sample/SampleApplication.java
+++ src/main/java/com/tejuskandipilli/sample/SampleApplication.java
@@ -2,12 +2,21 @@ package com.tejuskandipilli.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SampleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SampleApplication.class, args);
     }
+
+    @GetMapping
+    public String hello(){
+        return "Hello World!";
+    }
+
 
 }
---
+++ src/main/java/com/tejuskandipilli/sample/SoftwareEngineerRepository.java
@@ -0,0 +1,7 @@
+package com.tejuskandipilli.sample;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
